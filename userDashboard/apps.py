0630c469bafa39b25b409f8737f0461e39bc5e4d
gerald-x <geraldokolodc@gmail.com>
2023-06-24 12:39:26 +0100

    removed task imports

--- userDashboard/apps.py
+++ userDashboard/apps.py
@@ -4,8 +4,3 @@ from django.apps import AppConfig
 class UserdashboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'userDashboard'
-
-    def ready(self):
-        # Start the background task when the server starts
-        from trade.tasks import generate_profit_loss
-        generate_profit_loss()
